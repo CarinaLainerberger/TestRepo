f6cb14706da3ea97b521e7be711d4b268f309fbb
Carina Lainerberger <carina.lainerberger@htl-wels.at>
2018-10-18 11:22:07 +0200

    Add

---
+++ Regex_UE1/src/Regex1.java
@@ -0,0 +1,13 @@
+/* Erstellt von Carina Lainerberger
+   11.10.2018
+*/
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class Regex1 {
+    public static void main(String[] args) {
+        String eingabe="http://www.orf.at";
+        System.out.println(eingabe);
+        System.out.println(Pattern.matches("[h][t]{2}[p][s]?[:][/]{2}[w]{3}[.]{1}[a-zA-Z]{1,15}[.]{1}[a-z]{2,4}",eingabe));
+    }
+}
